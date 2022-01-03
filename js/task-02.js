b73a0699903ad32327d19c78f881f695d35f1812
Andriy Kryvopysha <logan1kry@gmail.com>
2022-01-03 20:03:10 +0200

    Task 1-3
    Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const vegetablesList = document.querySelector('#ingredients');
+
+ingredients.forEach(vegetable => {
+  const newItem = document.createElement("li");
+  newItem.textContent = vegetable;
+  newItem.classList.add('item');
+  vegetablesList.append(newItem);
+  })
+
+
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul.ingredients.
+
+
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const refreshedGall = document.querySelector('.gallery');
+
+const insert = images
+  .map((image) =>`<li><img src=${image.url} alt=${image.alt} width="350"></li>`).join("");
+
+refreshedGall.insertAdjacentHTML("beforeend", insert);
+  
+
+
+
+// Используй массив объектов images для создания элементов < img > вложенных в < li >.
+// Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
