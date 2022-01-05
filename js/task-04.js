294e7640d50904c62bb8cd8a12631203b8e9f5a1
Andriy Kryvopysha <logan1kry@gmail.com>
2022-01-05 13:40:18 +0200

    Just Start task4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const counterValue = document.querySelector('#value').textContent;
+
+console.log(counterValue)
+
+const increaseValueBtn = document.querySelector('[data-action="increment"]');
+console.log(increaseValueBtn)
+
+
+
+
+
+
+ // Создай переменную counterValue в которой будет храниться текущее значение счетчика и 
+ // инициализируй её значением 0.
+ // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение
+ // счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
