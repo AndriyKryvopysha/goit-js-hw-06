2c91ec3e4800ae602a7e96a059731c99950845a7
Andriy Kryvopysha <logan1kry@gmail.com>
2022-01-06 18:10:24 +0200

    HW completed 9Tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,22 @@
+const FormEl = document.querySelector('.login-form');
 
+function checkSubmit (event) {
+    event.preventDefault();
+    
+     const {
+    elements: { email, password }
+  } = event.currentTarget;
 
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+    }
+   
+    const result = {email: email.value, password: password.value} 
+    console.log(result);
+  event.currentTarget.reset();    
+} 
 
+FormEl.addEventListener('submit', checkSubmit)
 
 // Обработка отправки формы form.login-form должна быть по событию submit.
 // При отправке формы страница не должна перезагружаться.
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const changeColorBtn = document.querySelector('.change-color');
+
+const colorNameEl = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', changeBgColorRandom);
+
+function changeBgColorRandom() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorNameEl.textContent = getRandomHexColor()
+}
+
+
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на 
+// button.change - color и выводит значение цвета в span.color.
+// Для генерации случайного цвета используй функцию getRandomHexColor.
